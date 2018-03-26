096b6f22eef4b90ccfc1cb7dd6e2c9ce2b29e13f
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2018-03-27 00:07:12 +0200

    Fix bootstrapping. Add tables. Add hex to color name.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var color1;
+var color2;
 var lightnessFactor1;
 var lightnessFactor2;
 var contrastRatio;
@@ -45,6 +47,43 @@ function colorNameToHex(color) {
     else return '#' + color;
 }
 
+function hexToColorName(color) {
+    var colors = {"#f0f8ff":"aliceblue","#faebd7":"antiquewhite","#00ffff":"cyan","#7fffd4":"aquamarine",
+        "#f0ffff":"azure","#f5f5dc":"beige","#ffe4c4":"bisque","#000000":"black","#ffebcd":"blanchedalmond",
+        "#0000ff":"blue","#8a2be2":"blueviolet","#a52a2a":"brown","#deb887":"burlywood","#5f9ea0":"cadetblue",
+        "#7fff00":"chartreuse","#d2691e":"chocolate","#ff7f50":"coral","#6495ed":"cornflowerblue","#fff8dc":"cornsilk",
+        "#dc143c":"crimson","#00008b":"darkblue","#008b8b":"darkcyan","#b8860b":"darkgoldenrod",
+        "#a9a9a9":"darkgray","#006400":"darkgreen","#bdb76b":"darkkhaki","#8b008b":"darkmagenta",
+        "#556b2f":"darkolivegreen","#ff8c00":"darkorange","#9932cc":"darkorchid","#8b0000":"darkred",
+        "#e9967a":"darksalmon","#8fbc8f":"darkseagreen","#483d8b":"darkslateblue","#2f4f4f":"darkslategray",
+        "#00ced1":"darkturquoise","#9400d3":"darkviolet","#ff1493":"deeppink","#00bfff":"deepskyblue",
+        "#696969":"dimgray","#1e90ff":"dodgerblue","#b22222":"firebrick","#fffaf0":"floralwhite",
+        "#228b22":"forestgreen","#ff00ff":"magenta","#dcdcdc":"gainsboro","#f8f8ff":"ghostwhite","#ffd700":"gold",
+        "#daa520":"goldenrod","#808080":"gray","#008000":"green","#adff2f":"greenyellow","#f0fff0":"honeydew",
+        "#ff69b4":"hotpink","#cd5c5c ":"indianred","#4b0082":"indigo","#fffff0":"ivory","#f0e68c":"khaki",
+        "#e6e6fa":"lavender","#fff0f5":"lavenderblush","#7cfc00":"lawngreen","#fffacd":"lemonchiffon",
+        "#add8e6":"lightblue","#f08080":"lightcoral","#e0ffff":"lightcyan","#fafad2":"lightgoldenrodyellow",
+        "#d3d3d3":"lightgrey","#90ee90":"lightgreen","#ffb6c1":"lightpink","#ffa07a":"lightsalmon",
+        "#20b2aa":"lightseagreen","#87cefa":"lightskyblue","#778899":"lightslategray","#b0c4de":"lightsteelblue",
+        "#ffffe0":"lightyellow","#00ff00":"lime","#32cd32":"limegreen","#faf0e6":"linen",
+        "#800000":"maroon","#66cdaa":"mediumaquamarine","#0000cd":"mediumblue","#ba55d3":"mediumorchid",
+        "#9370d8":"mediumpurple","#3cb371":"mediumseagreen","#7b68ee":"mediumslateblue","#00fa9a":"mediumspringgreen",
+        "#48d1cc":"mediumturquoise","#c71585":"mediumvioletred","#191970":"midnightblue","#f5fffa":"mintcream",
+        "#ffe4e1":"mistyrose","#ffe4b5":"moccasin","#ffdead":"navajowhite","#000080":"navy","#fdf5e6":"oldlace",
+        "#808000":"olive","#6b8e23":"olivedrab","#ffa500":"orange","#ff4500":"orangered","#da70d6":"orchid",
+        "#eee8aa":"palegoldenrod","#98fb98":"palegreen","#afeeee":"paleturquoise","#d87093":"palevioletred",
+        "#ffefd5":"papayawhip","#ffdab9":"peachpuff","#cd853f":"peru","#ffc0cb":"pink","#dda0dd":"plum",
+        "#b0e0e6":"powderblue","#800080":"purple","#663399":"rebeccapurple","#ff0000":"red","#bc8f8f":"rosybrown",
+        "#4169e1":"royalblue","#8b4513":"saddlebrown","#fa8072":"salmon","#f4a460":"sandybrown","#2e8b57":"seagreen",
+        "#fff5ee":"seashell","#a0522d":"sienna","#c0c0c0":"silver","#87ceeb":"skyblue","#6a5acd":"slateblue",
+        "#708090":"slategray","#fffafa":"snow","#00ff7f":"springgreen","#4682b4":"steelblue","#d2b48c":"tan",
+        "#008080":"teal","#d8bfd8":"thistle","#ff6347":"tomato","#40e0d0":"turquoise","#ee82ee":"violet",
+        "#f5deb3":"wheat","#ffffff":"white","#f5f5f5":"whitesmoke","#ffff00":"yellow","#9acd32":"yellowgreen"};
+    if(!color.startsWith('#')) color = '#' + color;
+    if (typeof colors[color.toLowerCase()] != 'undefined') return colors[color.toLowerCase()];
+    else return "none";
+}
+
 function rgbToHex(rgb) {
     var r = parseInt(rgb.substring(0, rgb.indexOf(",")));
     var g = parseInt(rgb.substring(rgb.indexOf(",")+1, rgb.lastIndexOf(",")));
@@ -77,35 +116,32 @@ function calcContrastRatio(color1, color2) {
     return Math.round((lum1 >= lum2 ? (lum1 + 0.05) / (lum2 + 0.05) : (lum2 + 0.05) / (lum1 + 0.05)) * 100) / 100;
 }
 
-function changeLightnessFactor() {
+document.getElementById('mainDiv').addEventListener('input', function(){
+    color1 = shadeColor(colorNameToHex(document.getElementById('backgroundInput').value), lightnessFactor1);
+    color2 = shadeColor(colorNameToHex(document.getElementById('textInput').value), lightnessFactor2);
     lightnessFactor1 = 0.1 * document.getElementById('contrastSlider1').value;
     lightnessFactor2 = 0.1 * document.getElementById('contrastSlider2').value;
-}
+    contrastRatio = calcContrastRatio(color1, color2);
+    document.getElementById('textbg1').style.backgroundColor = color1;
+    document.getElementById('textbg1').style.color = color2;
+    document.getElementById('textbg3').style.backgroundColor = color1;
+    document.getElementById('textbg3').style.color = color2;
+    document.getElementById('textbg2').style.backgroundColor = color2;
+    document.getElementById('textbg2').style.color = color1;
+    document.getElementById('textbg4').style.backgroundColor = color2;
+    document.getElementById('textbg4').style.color = color1;
 
-function displayColor(listenerOn, listenerType, input1, input2, bgToColorize, textToColorize, toDisplayLuminance, toDisplayContrastRatio) {
-    document.getElementById(listenerOn).addEventListener(listenerType, function() {
-        changeLightnessFactor();
-        var color1 = shadeColor(colorNameToHex(document.getElementById(input1).value), lightnessFactor1);
-        var color2 = shadeColor(colorNameToHex(document.getElementById(input2).value), lightnessFactor2);
-        var relativeLuminance1 = calcLuminance(hexToRgb(colorNameToHex(color1)));
-        var relativeLuminance2 = calcLuminance(hexToRgb(colorNameToHex(color2)));
-        contrastRatio = calcContrastRatio(color1, color2);
-        if(bgToColorize != null && textToColorize == null) {
-            document.getElementById(bgToColorize).style.backgroundColor = color1;
-            document.getElementById(toDisplayLuminance).innerHTML = relativeLuminance1;
-        }
-        else if(textToColorize != null && bgToColorize == null) {
-            document.getElementById(textToColorize).style.color = color2;
-            document.getElementById(toDisplayLuminance).innerHTML = relativeLuminance2;
-        }
-        else {
-            document.getElementById(toDisplayLuminance).innerHTML = "Something went wrong here";
-        }
-        document.getElementById(toDisplayContrastRatio).innerHTML = "New Contrast Ratio: " + contrastRatio;
-    });
-}
+    document.getElementById('luminance1').innerHTML = calcLuminance(hexToRgb(colorNameToHex(color1))).toFixed(4);
+    document.getElementById('luminance2').innerHTML = calcLuminance(hexToRgb(colorNameToHex(color2))).toFixed(4);
 
-displayColor('contrastSlider1', 'input', 'backgroundInput', 'textInput', 'contrastDiv', null, 'backgroundLum', 'newContrastRatio');
-displayColor('backgroundInput', 'input', 'backgroundInput', 'textInput', 'contrastDiv', null, 'backgroundLum', 'newContrastRatio');
-displayColor('contrastSlider2', 'input', 'backgroundInput', 'textInput', null, 'newContrastRatio', 'textLum', 'newContrastRatio');
-displayColor('textInput', 'input', 'backgroundInput', 'textInput', null, 'newContrastRatio', 'textLum', 'newContrastRatio');
+    document.getElementById('name1').innerHTML = hexToColorName(color1);
+    document.getElementById('name2').innerHTML = hexToColorName(color2);
+
+    document.getElementById('hex1').innerHTML = colorNameToHex(color1);
+    document.getElementById('hex2').innerHTML = colorNameToHex(color2);
+
+    document.getElementById('rgb1').innerHTML = hexToRgb(colorNameToHex(color1));
+    document.getElementById('rgb2').innerHTML = hexToRgb(colorNameToHex(color2));
+
+    document.getElementById('contrastRatio').innerHTML = "Contrast ratio: " + contrastRatio;
+});
